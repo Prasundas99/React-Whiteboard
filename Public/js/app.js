cb2c9f9458c6911c0ec782611937fa363c268c6f
Prasundas99 <prasundas115@gmail.com>
2020-11-03 14:29:07 +0530

    refafctored  first page

--- Public/js/app.js
+++ Public/js/app.js
@@ -1,4 +1,18 @@
-//Sellecting elements of the commands
+//Working with Canvs api starts from here  
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+var widthRatio = 3;
+canvas.height = 120;
+canvas.width = canvas.height * widthRatio;
+
+
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+/*//Sellecting elements of the commands
 document.querySelectorAll("[data-command]").forEach(
      item => {
           item.addEventListener("click", (e)=>{
@@ -42,16 +56,43 @@ document.querySelectorAll("[data-color]").forEach(
 );
 
 
-//Working with Canvs api starts from here  
-const canvas = document.querySelector("#canvas");
-const cntx = canvas.getContext("2D");
+
 
 let painting = false;
   
-  function startposition(){
+  function startposition(e){
        painting = true;
+       draw(e);
   }
 
   function finishedposition(){
        painting=false;
+       ctx.beginPath();
   }
+
+  function draw(e) {
+       if(!painting) return;
+       ctx.lineWidth = 5;
+       ctx.lineCap = "round";
+     ctx.fillStyle = "#FF0000";
+       ctx.lineTo(e.clientX , e.ClientY);
+       ctx.stroke();
+       ctx.beginPath();
+       ctx.moveTo(e.clientX , e.clientY);
+  }
+
+  canvas.addEventListener("mousedown",startposition);
+  canvas.addEventListener("mouseup", finishedposition);
+  canvas.addEventListener("mousemove", draw);
+
+
+
+  ctx.moveTo(0, 0);
+  ctx.lineTo(200, 100);
+  ctx.stroke();
+
+
+ */
+ 
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0, 0, 150, 75);
